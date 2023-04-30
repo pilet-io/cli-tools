8efcdc0687e41cd77267946c05dc62e0e9517de4
Alex Potapov <potapov@yahoo.com>
2023-04-30 08:14:37 -0400

    init-ovh

--- init/install.sh
+++ init/install.sh
@@ -4,7 +4,7 @@ apt update
 apt install -y python3-pip
 pip install deterministic-rsa-keygen
 
-git clone git@github.com:pilet-io/cli-tools.git
+git clone https://github.com/pilet-io/cli-tools.git
 mv cli-tools ~/cli
 
 cat <<EOF >> ~/.profile
